683606c959884784db2709f2b43aec1bb366e1ac
Dirlandia404 <dirlandiaoliveira@alu.ufc.br>
2020-06-04 19:15:24 -0300

    inicio

---
+++ gitInit.sh
@@ -0,0 +1,6 @@
+git init
+git config user.name "Dirlandia404"
+git config user.email "dirlandiaoliveira@alu.ufc.br"
+echo "Nome do repositorio: "
+read repositorio
+git remote add origin https://github.com/Dirlandia404/$repositorio
