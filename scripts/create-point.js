aef828921a6ebec362aa091ddbfe4425a211b996
Dirlandia404 <dirlandiaoliveira@alu.ufc.br>
2020-06-06 21:19:45 -0300

    aula03

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -27,12 +27,18 @@ function getCities(event){
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
 
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
+
+
     fetch(url)
     .then(res => res.json() )
     .then(cities => {
 
         for(const city of cities){
-            citySelect.innerHTML += `<option value= "${city.id}">${city.nome}</option`
+            citySelect.innerHTML += `<option value= "${city.nome}">${city.nome}</option`
         }
 
         citySelect.disabled = false
@@ -46,4 +52,52 @@ document
     .querySelector("select[name=uf")
     .addEventListener("change", getCities)
 
+/*itens de coleta 
+pegar todos os li */
 
+const itensToCollect = document.querySelectorAll(".itens-grid li")
+
+for(const item of itensToCollect){
+    item.addEventListener("click", handleSelecteditem)
+}
+const collectedItens = document.querySelector("input[name=itens]")
+
+let selectedItens = []
+function handleSelecteditem(event){
+    const itemLi = event.target
+
+    //add ou remover classe js
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+
+
+    
+    /*Verificar se existem itens selecionados, se sim pegar os itens selecionados 
+    */
+
+
+    const alreadySelected = selectedItens.findIndex(item => {
+        const itemFound = item == itemId
+        return itemFound
+        
+    })
+    //se já estiver selecionado tirar seleção
+
+    if (alreadySelected >= 0) {
+        //tirar seleção
+        const filteredItens = selectedItens.filter(item =>{
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        selectedItens = filteredItens
+    }
+    else{
+        // se nao addicona
+        selectedItens.push(itemId)
+        
+    }
+    console.log(selectedItens)
+    //atualizar o campo
+
+    collectedItens.value = selectedItens
+}
